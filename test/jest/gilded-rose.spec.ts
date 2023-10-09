bf6b028e0020def7017fd7ab81dadee224cc3ffa
Emily Choi <emily.choi@f4s.com>
2023-10-09 17:43:35 +1300

    Add unit tests for Aged Brie item

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,25 @@
 import { Item, GildedRose } from '../../app/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+  describe('Aged Brie', () => {
+    it ('Quality cannot be negative', () => {
+      const gildedRose = new GildedRose([new Item("Aged Brie", 1, -2)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    });
+
+    it('Should increase quality by one when quality is less than 50', () => {
+      const gildedRose = new GildedRose([new Item("Aged Brie", 2, 40)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(1);
+      expect(items[0].quality).toBe(41);
+    });
+
+    it('Should not increase quality anymore when quality is more than 50', () => {
+      const gildedRose = new GildedRose([new Item("Aged Brie", 2, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(1);
+      expect(items[0].quality).toBe(50);
+    });
   });
 });
